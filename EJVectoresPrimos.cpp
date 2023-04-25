a66107430af47e6b373c787eb5e13da03c05c016
Nicole <96841120+nicoleviarengo@users.noreply.github.com>
2023-04-25 12:55:39 -0300

    Add files via upload

---
+++ EJVectoresPrimos.cpp
@@ -0,0 +1,53 @@
+/*Cargar numeros en un vector
+Armar otro vector con los numeros primos
+Buscar el mayor de los numeros primos
+Mostrar */
+
+#include <stdio.h>
+#include <conio.h>
+
+int vector[50];
+int primos[50];
+void buscarprimos(int n);
+int main()
+{
+	int n;
+	printf("Ingrese la cantidad de numeros: ");
+	scanf("%d", &n);
+
+	buscarprimos(n);
+	
+}
+void buscarprimos(int n)
+{
+	int s=0, k=0, c=-1,cont;
+	
+	for(int k=0; k<n; k++)
+	{
+		printf("Ingrese los numeros del vector: ");
+		scanf("%d", &vector[k]);
+	}
+	
+	for(int i=2; i<vector[k]-1; i++)
+	{
+		if(vector[k]%2==0)
+		{
+			s=1;
+		}
+	}
+	cont=0;
+	if(s==0)
+	{
+		c++;
+		primos[c]=vector[k];
+		cont++;
+	}
+	
+	printf("\nCantidad de primos: %d", cont);
+	printf("\nPrimos: ");
+	for(int j=0; j<cont; j++)
+	{
+		printf(" %d ", primos[j]);
+	}
+}
+	
---
+++ Invertir vector.cpp
@@ -0,0 +1,41 @@
+/*Escribir un programa que lea del teclado un vector de 10 números
+enteros, lo invierta y finalmente lo muestre de nuevo.*/
+
+#include <stdio.h>
+# include <conio.h>
+#define N 10
+int vector [N];
+void invertir();
+
+int main()
+{
+	int i;
+	
+	printf( "\nIntroduzca 10 numeros enteros:\n\t" );
+	for(i=0;i<10;i++)
+	{
+		scanf( "%d%*c", &vector[i] );
+		printf( "\t" );
+	}
+	printf( "\nEl vector original es:\n\t" );
+	for(i=0;i<10;i++)
+		printf( "v[%d]=%d\n\t", i, vector[i] );
+	
+	invertir();
+	
+	printf( "\nEl vector invertido es:\n\t" );
+	for(i=0;i<10;i++)
+		printf( "v[%d]=%d\n\t", i, vector[i] );
+}
+
+void invertir ()
+{
+	int i,tmp;
+	for(i=0;i<N/2;i++)
+	{
+		tmp=vector[i];
+		vector[i]=vector[9-i];
+		vector[9-i]=tmp;
+	}
+	
+}
